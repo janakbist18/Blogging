83b387b2ea93e8a6bc3a6bdc2ef9ee500209cd85
janakbista <janakbist64@gmail.com>
2026-05-25 13:27:23 +0545

    post by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, "home.html")
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
+    posts = Blog.objects.filter(status='Published').order_by('-created_at')
+
+
+
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'posts': posts,
+
+    }
+
+    return render(request, "home.html", context)
+
+
+def favicon(request):
+        svg = """<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'>
+    <rect width='64' height='64' rx='14' fill='#111827'/>
+    <path d='M18 22h28v6H18zm0 10h28v6H18zm0 10h18v6H18z' fill='#f59e0b'/>
+</svg>"""
+        return HttpResponse(svg, content_type='image/svg+xml')
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request, category_id):
+
+    posts = Blog.objects.filter(category=category_id, status = 'Published')
+    try:
+        category = Category.objects.get(id=category_id)
+    except:
+        return redirect('home')
+
+    # category = get_object_or_404(Category, id=category_id)
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
